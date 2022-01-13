21992f423a3055c001813d4baa3db5b94ea81cf5
BekaKavlelashvili <beqakavlela2001@gmail.com>
2022-01-13 07:34:32 -0800

    add homework 6

---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Homework6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region exercise 1.
+            //int number = int.Parse(Console.ReadLine());
+            //int[] numbersArray = new int[number];
+            //for(int i = 0; i < numbersArray.Length; i++)
+            //{              
+            //    numbersArray[i] = i+1;
+            //}
+            //foreach(double item in numbersArray)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("-------");
+            //Console.WriteLine("Even");
+
+            //int[] even = new int[number];
+            //for(int j = 0; j < even.Length; j+=2)
+            //{
+            //    even[j] = j+2;
+            //}
+            //foreach(int ev in even)
+            //{
+            //    Console.WriteLine(ev);
+            //}
+
+            //Console.WriteLine("-------");
+            //Console.WriteLine("Odd");
+
+            //int[] odd = new int[number];
+            //for (int j = 0; j < odd.Length; j+=2)
+            //{
+            //    even[j] = j+1;
+            //}
+            //foreach (int od in even)
+            //{
+            //    Console.WriteLine(od);
+            //}
+
+            #endregion
+
+
+
+        //TODO: EXERCISE 2 AND 3. ვერც ვასწრებ, მთელი დღე დენი არ მქონდა :დდ თან ვერც გავიაზრე ლინქი ნორმალურად რა.
+           
+        }
+    }
+}
