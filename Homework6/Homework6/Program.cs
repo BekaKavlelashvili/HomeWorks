3895e3eb17031e11a46a87a1385f81851e743b47
BekaKavlelashvili <beqakavlela2001@gmail.com>
2022-01-16 09:41:30 -0800

    add homework 7

--- Homework6/Homework6/Program.cs
+++ Homework6/Homework6/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections.Generic;
 namespace Homework6
 {
-    internal class Program
+    internal class DateTim
     {
         static void Main(string[] args)
         {
+            //chemeburad dawerili pirveli davaleba
             #region exercise 1.
             //int number = int.Parse(Console.ReadLine());
             //int[] numbersArray = new int[number];
-            //for(int i = 0; i < numbersArray.Length; i++)
-            //{              
-            //    numbersArray[i] = i+1;
+            //for (int i = 0; i < numbersArray.Length; i++)
+            //{
+            //    numbersArray[i] = i + 1;
             //}
-            //foreach(double item in numbersArray)
+            //foreach (double item in numbersArray)
             //{
             //    Console.WriteLine(item);
             //}
@@ -21,11 +23,11 @@ namespace Homework6
             //Console.WriteLine("Even");
 
             //int[] even = new int[number];
-            //for(int j = 0; j < even.Length; j+=2)
+            //for (int j = 0; j < even.Length; j += 2)
             //{
-            //    even[j] = j+2;
+            //    even[j] = j + 2;
             //}
-            //foreach(int ev in even)
+            //foreach (int ev in even)
             //{
             //    Console.WriteLine(ev);
             //}
@@ -34,9 +36,9 @@ namespace Homework6
             //Console.WriteLine("Odd");
 
             //int[] odd = new int[number];
-            //for (int j = 0; j < odd.Length; j+=2)
+            //for (int j = 0; j < odd.Length; j += 2)
             //{
-            //    even[j] = j+1;
+            //    even[j] = j + 1;
             //}
             //foreach (int od in even)
             //{
@@ -46,9 +48,90 @@ namespace Homework6
             #endregion
 
 
+            //leqciaze axsnilis mixedvit dawerili.
+            #region exercise 1.
+            //Console.WriteLine("Enter array size: ");
+            //int array_size = Convert.ToInt32(Console.ReadLine());
+            //int[] arr1 = new int[array_size];
+            //int[] arr2 = new int[array_size];
+            //int[] arr3 = new int[array_size];
+            //int i, evenindex = 0, oddindex = 0;
+            //for (i = 0; i < arr1.Length; i++)
+            //{
+            //    Console.WriteLine($"Element - {i} : ");
+            //    arr1[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //for (i = 0; i < arr1.Length; i++)
+            //{
+            //    if (arr1[i] % 2 == 0)
+            //    {
+            //        arr2[evenindex] = arr1[i];
+            //        evenindex++;
+            //    }
+            //    else
+            //    {
+            //        arr3[oddindex] = arr1[i];
+            //        oddindex++;
+            //    }
+            //}
+            //Console.WriteLine("\n Even elements: \n");
+            //for (i = 0; i < arr1.Length; i++)
+            //{
+            //    if (arr2[i] != 0)
+            //    {
+            //        Console.WriteLine($"{arr2[i]}");
+            //    }
 
-        //TODO: EXERCISE 2 AND 3. ვერც ვასწრებ, მთელი დღე დენი არ მქონდა :დდ თან ვერც გავიაზრე ლინქი ნორმალურად რა.
-           
+            //}
+            //Console.WriteLine($"\n Odd elements: \n");
+            //for (i = 0; i < arr2.Length; i++)
+            //{
+            //    if (arr3[i] != 0)
+            //    {
+            //        Console.WriteLine($"{arr3[i]}");
+            //    }
+
+            //}
+            #endregion
+
+            //es titqmis gadawerilia leqciidan, magram tan viazreb, prosta ar vwer..
+            #region exercise 2.
+            //int[] myArr = new int[] { 1, 2,  4, 8, 3, 7, 0, 5, 7, 11, 15, 2, 4, 3, 11, 3, 8, 5, 0, 2, 7 };
+
+            //var m = from x in myArr
+            //        group x by x into y
+            //        select y;
+
+            //foreach(var element in m)
+            //{
+            //    Console.WriteLine( element.Key + "\t" + element.Sum() + "\t\t\t" + element.Count());
+            //}
+            //Console.WriteLine();
+            #endregion
+
+            //esec.
+            #region exercise 3.
+            //List<int> templist = new List<int>
+            //{
+            //    5,
+            //    3,
+            //    1,
+            //    8,
+            //    11,
+            //    24,
+            //    12,
+            //    14,
+            //    30
+            //};
+            //Console.WriteLine("How many records  you want to display? ");
+            //int quantity = Convert.ToInt32(Console.ReadLine());
+            //templist.Sort();
+            //templist.Reverse();
+            //foreach(int topn in templist.Take(quantity))
+            //{
+            //    Console.WriteLine(topn);
+            //}
+            #endregion
         }
     }
 }
---
+++ homework7/homework7/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace homework7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region exercise1.  
+            //var s2 = Convert.ToInt32(Console.ReadLine());//didi kvadratis gverdi;
+            //var s1 = s2 / Math.Sqrt(2);//patara kvadratis gverdi= didi kvadratis gverdi/fesvi 2-ze;
+            //var difference = Math.Pow(s2, 2) - Math.Pow(s1, 2);
+            //Console.WriteLine(difference);
+            #endregion
+
+            #region exercise 2.
+            //string user, user1, user2, user3, user4;
+            //string[] jackpot = new string[] { "$", "$", "$", "$", "$" };
+            //user = Console.ReadLine();
+            //user1 = Console.ReadLine();
+            //user2 = Console.ReadLine();
+            //user3 = Console.ReadLine();
+            //user4 = Console.ReadLine();
+
+            //for (var j = 0; j < jackpot.Length; j++)
+            //{
+            //    if (user == jackpot[j] && user1 == jackpot[j + 1] && user2 == jackpot[j + 2] && user3 == jackpot[j + 3]
+            //                       && user4 == jackpot[j + 4])
+            //    {
+            //        Console.WriteLine("You won 500000000 $");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Try again, loooser");
+            //        break;
+            //    }
+            //}
+            #endregion
+
+            #region exercise 3.
+            //Console.WriteLine("Enter win number:");
+            //int win = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter draw number:");
+            //int draw = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter lose numebr:");
+            //int lose = Convert.ToInt32(Console.ReadLine());
+
+            //int total_win = win * 3;
+            //Console.WriteLine($"Win score = {total_win}");
+            //int total_draw = draw;
+            //Console.WriteLine($"draw score = {total_draw}");
+            //int total_lose = lose * 0;
+            //Console.WriteLine($"lose score = {total_lose}");
+            //int total_score = total_win + total_draw + total_lose;
+            //Console.WriteLine($"Total score {total_score}");
+
+            #endregion
+
+            #region exercise 4.  
+            //int normal_workday = 8;
+            //int salary_per_hour = 10;
+            //int salary_per_overtime_hour = 15;
+            //int weekend_salary_per_hour = 20;
+            //int normal_salary_per_day = 80;
+
+            //Console.WriteLine("Enter your monday working hours");
+            //int monday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your tuesday working hours");
+            //int tuesday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your wednesday working hours");
+            //int wednesday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your thursday working hours");
+            //int thursday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your friday working hours");
+            //int friday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your saturday working hours");
+            //int saturday = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your sunday working hours");
+            //int sunday = Convert.ToInt32(Console.ReadLine());
+
+            //int m, t, w, th, fr;
+            //int sat = saturday * weekend_salary_per_hour;
+            //int sun = sunday * weekend_salary_per_hour;
+            //if (true)
+            //{
+            //    if (monday > normal_workday)
+            //    {
+            //         m = (monday - normal_workday) * salary_per_overtime_hour + normal_salary_per_day;
+            //    }
+            //    else
+            //    {
+            //         m = monday * salary_per_hour;
+            //    }
+            //    if (tuesday > normal_workday)
+            //    {
+            //         t = (tuesday - normal_workday) * salary_per_overtime_hour + normal_salary_per_day;
+            //    }
+            //    else
+            //    {
+            //         t = tuesday * salary_per_hour;
+            //    }
+            //    if (wednesday > normal_workday)
+            //    {
+            //         w = (wednesday - normal_workday) * salary_per_overtime_hour + normal_salary_per_day;
+            //    }
+            //    else
+            //    {
+            //         w = wednesday * salary_per_hour;
+            //    }
+            //    if (thursday > normal_workday)
+            //    {
+            //         th = (thursday - normal_workday) * salary_per_overtime_hour + normal_salary_per_day;
+            //    }
+            //    else
+            //    {
+            //         th = thursday * salary_per_hour;
+
+            //    }
+            //    if (friday > normal_workday)
+            //    {
+            //         fr = (friday - normal_workday) * salary_per_overtime_hour + normal_salary_per_day;
+            //    }
+            //    else
+            //    {
+            //         fr = friday * salary_per_hour;
+            //    }
+            //    Console.WriteLine("---------------");
+            //    Console.WriteLine("your salary is - ");
+            //    Console.WriteLine(m + t + w + th + fr + sat + sun);
+            //}
+            #endregion
+
+            //am logikit mivyevi  magram konsolshi progress ver  vigeb :/.
+            #region execise 5.
+            //int run_days = Convert.ToInt32(Console.ReadLine());
+            //int progress_days = 0;
+            //int[] vs = new int[run_days];
+            //for (int i = 0; i < run_days; i++)
+            //{
+            //    int per_day = Convert.ToInt32(Console.ReadLine());
+            //    if (vs[i] < vs[i + 1])
+            //    {
+            //        vs[i + 1] = progress_days;
+            //        Console.WriteLine(progress_days);
+            //    }
+            //}
+            #endregion
+
+
+            #region exercise 6.
+            //Console.WriteLine("Enter character's number :");
+            //int user = Convert.ToInt32(Console.ReadLine());
+            //string[] myWords = new string[] { "Hello", "World", "Communication", "Private" };
+            //int hello = myWords[0].Length;
+            //int world = myWords[1].Length;
+            //int comm = myWords[2].Length;
+            //int privat = myWords[3].Length;
+            //if (hello >= user)
+            //{
+            //    Console.WriteLine(myWords[0]);
+            //}
+            //if (world >= user)
+            //{
+            //    Console.WriteLine(myWords[1]);
+            //}
+            //if (comm >= user)
+            //{
+            //    Console.WriteLine(myWords[2]);
+            //}
+            //if (privat >= user)
+            //{
+            //    Console.WriteLine(myWords[3]);
+            //}
+            //if(user > world && user > hello && user > comm && user> privat)
+            //{
+            //    Console.WriteLine("Unsuccessful search");
+            //}
+            //Console.WriteLine("====================");
+            //Console.WriteLine("Enter any key to exit");
+            #endregion
+        }
+    }
+}
